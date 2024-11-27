135571fcb8b5d7d5ddf877451502c4f9b751cc94
liviaBELLO <livia_bello@outlook.com>
2024-11-27 09:24:31 -0300

    cors-2

--- api-post/src/main/java/com/moots/api_post/configuration/CorsConfiguration.java
+++ api-post/src/main/java/com/moots/api_post/configuration/CorsConfiguration.java
@@ -6,12 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            }
+        };
     }
 }
